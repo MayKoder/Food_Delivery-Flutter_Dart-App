44c547394837f4a5678e67572d5bcde3f0381452
Jordi Pardo <jordipardogutierrez@gmail.com>
2020-11-08 02:42:07 +0100

    Update food_info_screen.dart

--- Food_Delivery_App/lib/screens/food_info_screen.dart
+++ Food_Delivery_App/lib/screens/food_info_screen.dart
@@ -4,14 +4,12 @@ class FoodInfoScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
         body: Column(
-          
       children: [
         Expanded(
           flex: 1,
           child: _FoodInfo(),
-          ),
+        ),
       ],
     ));
   }
@@ -25,24 +23,89 @@ class _FoodInfo extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-
-          Expanded(flex: 1, child: _TopBar()),
-          Expanded(flex: 2, child: _UserInfo()),
-          Expanded(flex: 1, child: Text("data3")/* Search Bar*/),
-          Expanded(flex: 1, child: Column(children: [
-            Row(children: [_CreateButton("Burger"),_CreateButton("Pizza"),_CreateButton("Sandwitch")],),
-            Row(children: [Text("item1"),Text("item2"),],),
-          ],)),
-          Expanded(flex: 1, child: Text("data5")),
-          Expanded(flex: 1, child: Text("data6")),
-          Expanded(flex: 1, child: Text("data7")),
+          Expanded(flex: 2, child: _TopBar()),
+          Expanded(flex: 4, child: _UserInfo()),
+          Expanded(flex: 2, child: Text("data3") /* Search Bar*/),
+          Expanded(
+              flex: 3,
+              child: Column(children: [
+                Row(
+                  children: [
+                    _CreateButton("Burger", autoFocused: true),
+                    SizedBox(width: 24),
+                    _CreateButton("Pizza"),
+                    SizedBox(width: 24),
+                    _CreateButton("Sandwitch")
+                  ],
+                ),
+                SizedBox(height: 15),
+                Row(
+                  children: [
+                    _CreateButton("Sauces"),
+                    SizedBox(width: 24),
+                    _CreateButton("Others"),
+                  ],
+                ),
+              ])),
+          Expanded(
+            flex: 1,
+            child: Container(
+              alignment: Alignment.centerRight,
+              child:
+                  Text("see more", style: TextStyle(color: Colors.orange[900])),
+            ),
+          ),
+          Expanded(flex: 4, child: Row(children: [Text("ham1"),Text("ham2")],)),
+          Expanded(
+            flex: 5,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.circle, size: 10, color: Colors.orange[900]),
+                SizedBox(width: 9),
+                Icon(Icons.circle, size: 10, color: Colors.grey[300]),
+                SizedBox(width: 9),
+                Icon(Icons.circle, size: 10, color: Colors.grey[300]),
+                SizedBox(width: 9),
+                Icon(Icons.circle, size: 10, color: Colors.grey[300]),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                Icon(Icons.home,size: 35,color: Colors.grey[100],),
+                Icon(Icons.favorite_outline,size: 35,color: Colors.grey[100],),
+                Icon(Icons.shopping_cart_outlined,size: 35,color: Colors.grey[100],),
+                Icon(Icons.person_outline,size: 35,color: Colors.grey[100],),
+              ],),
+              decoration: BoxDecoration(
+                  color: Colors.orange[900],
+                  borderRadius: BorderRadius.all(Radius.circular(60))),
+             ),
+          ),
         ],
       ),
-
     );
   }
 
-  FlatButton _CreateButton(String text) => FlatButton(child: Text(text),color: Colors.grey[200],hoverColor:Colors.orange[900] , onPressed: (){},);
+  FlatButton _CreateButton(String text, {bool autoFocused = false}) =>
+      FlatButton(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+        child: Text(
+          text,
+          style: TextStyle(
+              color: autoFocused ? Colors.grey[300] : Colors.grey[500]),
+        ),
+        autofocus: autoFocused,
+        focusColor: Colors.orange[900],
+        color: Colors.grey[200],
+        hoverColor: Colors.orange[700],
+        onPressed: () {},
+      );
 }
 
 class _TopBar extends StatelessWidget {
@@ -51,10 +114,17 @@ class _TopBar extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Icon(Icons.list,size: 36,),
-        Icon(Icons.shopping_cart_outlined,size: 36,color: Colors.grey,),
-
-    ],);
+        Icon(
+          Icons.list,
+          size: 36,
+        ),
+        Icon(
+          Icons.shopping_cart_outlined,
+          size: 36,
+          color: Colors.grey,
+        ),
+      ],
+    );
   }
 }
 
@@ -64,9 +134,12 @@ class _UserInfo extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-      Text("Hi David,",style: TextStyle(fontSize: 26,fontWeight: FontWeight.bold)),
-      Text("Let's enjoy quality food",style: TextStyle(fontSize: 20,color: Colors.grey)),
-    ],);
+        Text("Hi David,",
+            style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold)),
+        Text("Let's enjoy quality food",
+            style: TextStyle(fontSize: 20, color: Colors.grey)),
+      ],
+    );
   }
 }
 
@@ -74,7 +147,6 @@ class _FoodPreview extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
       color: Colors.amber,
     );
   }
