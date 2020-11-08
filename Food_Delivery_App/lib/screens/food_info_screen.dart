f281d8b1fe607b6c9b1a2e88538eec74878a49fc
MayKoder <sg.miquel@gmail.com>
2020-11-08 15:02:43 +0100

    Searchbar added

--- Food_Delivery_App/lib/screens/food_info_screen.dart
+++ Food_Delivery_App/lib/screens/food_info_screen.dart
@@ -29,17 +29,44 @@ class _FoodInfo extends StatelessWidget {
           Expanded(flex: 2, child: _TopBar()),
           Expanded(flex: 4, child: _UserInfo()),
           Expanded(
-            flex: 2,
-            child: Row(
-              children: [
-                Icon(Icons.search),
-                // FlatButton(
-                //   onPressed: (){},
-                //   child: Icon(Icons.filter_list_outlined, color: Colors.white,),
-                //   color: Colors.orange[900],
-                //   shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-                // )
-              ],
+            flex: 2, 
+            child: 
+            Container(
+              margin: const EdgeInsets.only(top: 15.0, bottom: 15.0),
+                padding: const EdgeInsets.only(left: 15),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey[400]),
+                  borderRadius: BorderRadius.all(Radius.circular(50.0),
+                  ), //  
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  SizedBox(height: double.infinity, child: Icon(Icons.search)),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        focusedBorder: InputBorder.none,
+                        errorBorder: InputBorder.none,
+                        disabledBorder: InputBorder.none,
+                        border: InputBorder.none,
+                        contentPadding:EdgeInsets.only(left: 15, bottom: 11, top: 11, right: 15), 
+                        hintText: "Search place",
+                      ),
+                    ),
+                  ),
+                  FlatButton(
+                    onPressed: (){}, 
+                    child: Icon(Icons.filter_list_outlined, color: Colors.white), 
+                    color: Colors.orange[900],
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                          bottomRight: Radius.circular(50.0),
+                          topRight: Radius.circular(50.0))),
+                  )
+                ],
+              ),
             ),
           ),
           Expanded(
